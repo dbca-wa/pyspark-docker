9516fe016412bd51c966101e647a2705b116faa2
Ashley Felton <ashley@ropable.com>
2022-05-24 09:36:09 +0800

    Added http_requests_single_host.py script.

--- http_requests_single_host.py
+++ http_requests_single_host.py
@@ -1,29 +1,23 @@
-from azure.storage.blob import ContainerClient, BlobSasPermissions, generate_blob_sas
 import argparse
 import csv
-from datetime import date, datetime, timedelta
-from email.message import EmailMessage
+from datetime import datetime, timedelta
 from log4j4py import Log4JProxyHandler
 import logging
 import os
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, TimestampType, StringType, IntegerType
-from smtplib import SMTP
 import sys
 
 
 STORAGE_ACCOUNT_NAME = os.environ['STORAGE_ACCOUNT_NAME']
 STORAGE_ACCOUNT_KEY = os.environ['STORAGE_ACCOUNT_KEY']
-SMTP_SERVER = os.environ['SMTP_SERVER']
-EMAIL_REPORT_SENDER = os.environ['EMAIL_REPORT_SENDER']
-EMAIL_REPORT_RECIPIENTS = os.environ['EMAIL_REPORT_RECIPIENTS'].split(',')
 
 logging.basicConfig(
     stream=sys.stdout,
     level=logging.INFO,
     format="%(asctime)s %(levelname)s %(message)s",
 )
-LOGGER = logging.getLogger("OracleFinancialsLogins")
+LOGGER = logging.getLogger("HttpRequestsSingleHost")
 
 
 def spark_session():
@@ -71,81 +65,49 @@ def read_nginx_logs(hours_ago, session, blob_container="access-logs"):
     return df
 
 
-def oracle_financials_logs(df):
-    """Apply filters to the DataFrame to only include requests to the Oracle Financials login path.
-    """
-    df = df.filter((df.host == "oraclefinancials.dbca.wa.gov.au") & (df.path == "/OA_HTML/AppsLogin"))
-    df = df.orderBy(df.timestamp)
-
+def exclude_requests(df):
+    # Exclude requests from Nessus
+    df = df.filter(df.remote_ip != "10.6.20.97")
+    # Exclude requests from PRTG
+    df = df.filter(df.user_agent != "Mozilla/5.0 (compatible; PRTG Network Monitor (www.paessler.com); Windows)")
     return df
 
 
-def upload_report_blob(df, hours_ago, start_date, blob_container="analytics"):
-    """For the passed-in DataFrame, write out the contents to a CSV and upload to blob storage.
+def filter_requests(df, host):
+    """Apply filters to the DataFrame to only include requests to host of interest.
+    """
+    df = df.filter((df.host == host) & (df.email != ""))
+    df = df.orderBy(df.timestamp)
+    return df
+
+
+def write_report(df, filename):
+    """For the passed-in DataFrame, write out the contents to a CSV.
     """
     df = df.coalesce(1)
-    temp_file = open("/tmp/placeholder.csv", "a+")
-
+    temp_file = open(f"/out/{filename}", "w+")
     fieldnames = df.columns
     writer = csv.DictWriter(temp_file, fieldnames)
     writer.writerow(dict(zip(fieldnames, fieldnames)))
+
     for row in df.toLocalIterator():
         writer.writerow(row.asDict())
-
-    temp_file.seek(0)
-
-    # Write the output to CSV in a defined blob container.
-    day_count = int(hours_ago / 24)
-    ds = start_date.strftime("%Y-%m-%d")
-    blob_name = f"oracle_financials_logins/oracle_financials_logins_{day_count}_day_{ds}.csv"
-    container_client = ContainerClient(
-        account_url=f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net",
-        container_name=blob_container,
-        credential=STORAGE_ACCOUNT_KEY,
-    )
-    container_client.upload_blob(name=blob_name, data=temp_file.read(), overwrite=True)
-
-    return blob_name
-
-
-def send_email_report(blob, container="analytics"):
-    """For a passed-in blob, generate a SAS token and email recipients a link to the blob.
-    """
-    sas_token = generate_blob_sas(
-        account_name=STORAGE_ACCOUNT_NAME,
-        container_name=container,
-        blob_name=blob,
-        account_key=STORAGE_ACCOUNT_KEY,
-        permission=BlobSasPermissions(read=True),
-        expiry=datetime.utcnow() + timedelta(days=365 * 3),  # Three years.
-        start=datetime.utcnow(),
-    )
-    blob_url = f"https://{STORAGE_ACCOUNT_NAME}.blob.core.windows.net/{container}/{blob}?{sas_token}"
-    recipients = EMAIL_REPORT_RECIPIENTS
-    smtp = SMTP(SMTP_SERVER)
-
-    for recipient in recipients:
-        # Construct the email.
-        email = EmailMessage()
-        email["Subject"] = "Oracle Financials access logs report"
-        email["From"] = EMAIL_REPORT_SENDER
-        email["To"] = recipient
-        email.set_content(f'See attachment <a href="{blob_url}">{blob}</a>', subtype='html')
-        # Send the message via Postmark SMTP server.
-        smtp.send_message(email)
-        LOGGER.info(f"Sending email to {recipient}")
 
 
 if __name__ == "__main__":
     all_args = argparse.ArgumentParser()
     all_args.add_argument("--hours", action="store", type=int, required=True, help="Number of hours into the past to load Nginx logs")
+    all_args.add_argument("--host", action="store", type=str, required=True, help="Hostname to filter HTTP request for")
+    all_args.add_argument("--filename", action="store", type=str, required=True, help="Filename for the CSV report output")
     args = vars(all_args.parse_args())
     hours_ago = int(args["hours"])
+    host = str(args["host"])
+    filename = str(args["filename"])
     session = spark_session()
     pyspark_handler = Log4JProxyHandler(session)
     LOGGER.addHandler(pyspark_handler)
     LOGGER.info("Starting report generation")
     df = read_nginx_logs(hours_ago, session)
-    df = oracle_financials_logs(df)
-    blob_name = upload_report_blob(df, hours_ago, date.today())
-    send_email_report(blob_name)
+    df = exclude_requests(df)
+    df = filter_requests(df, host)
+    write_report(df, filename)
